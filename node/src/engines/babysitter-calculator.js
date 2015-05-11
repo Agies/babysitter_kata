3ffa35898a7ebe0fd4edd2d6444630377798da0b
Jerel R Hass <agies1@hotmail.com>
2015-05-11 01:21:20 -0400

    require bedtime

--- node/src/engines/babysitter-calculator.js
+++ node/src/engines/babysitter-calculator.js
@@ -3,12 +3,13 @@ var moment = require("moment"),
 calculator.timeMatch = /^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9]))?([A,P,a,p][M,m])?$/;
 calculator.minStartTime = 17;
 calculator.maxLeaveTime = 4;
-calculator.calculate = function(startTime, leaveTime) {
+calculator.calculate = function(startTime, leaveTime, bedTime) {
 	'use strict';
 	var parsedStartTime,
 		parsedLeaveTime;
 	if (!startTime) throw "start time required";
-	if (!leaveTime) throw "start time required";
+	if (!leaveTime) throw "leave time required";
+	if (!bedTime) throw "bed time required";
 	parsedStartTime = moment(startTime, "hh:mma");
 	parsedLeaveTime = moment(leaveTime, "hh:mma");
 	if (!calculator.timeMatch.test(startTime)) throw "start time must be valid";
--- node/test/tests.js
+++ node/test/tests.js
@@ -9,20 +9,20 @@ describe('Baby sitter calculator', function(){
     assert.equal(typeof sut.calculate, 'function');
   });
   describe('calculate function', function(){
-    it('should require a start time AND leave time', function(){
-      sut.calculate("17:00", "17:00");
+    it('should require a start time AND leave time AND bed time', function(){
+      sut.calculate("17:00", "17:00", "17:00");
     });
     it('should require a start time to be parsable', function(){
-      sut.calculate("6:00PM", "1:00");
+      sut.calculate("6:00PM", "1:00", "8:00PM");
     });
     it('should require a start time to be no earlier than 5PM', function(){
-      sut.calculate("5:00PM", "1:00");
+      sut.calculate("5:00PM", "1:00", "8:00PM");
     });
     it('should require a leave time to be parsable', function(){
-      sut.calculate("6:00PM", "1:00");
+      sut.calculate("6:00PM", "1:00", "8:00PM");
     });
     it('should require a leave time to be no later than 4AM the following day', function(){
-      sut.calculate("5:00PM", "4:00");
+      sut.calculate("5:00PM", "4:00", "8:00PM");
     }); 
   })
 });
